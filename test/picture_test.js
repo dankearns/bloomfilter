64bee786a19718b014c7fe149aab401a7383977c
Dan Kearns <dan@thekearns.org>
2011-10-28 23:08:06 -0700

    pretty pictures

---
+++ test/picture_test.js
@@ -0,0 +1,34 @@
+var BloomFilter = require('../bloomfilter').BloomFilter;
+var vows = require('vows');
+var assert = require('assert');
+var _ = require('underscore');
+
+var v = vows.describe("bloomfilter pictures").addBatch({
+    "a bloom filter with entries": {
+        topic: function() {
+            var f = new BloomFilter(10,0.22);
+            var str = [ 27, "ZEBRA", "@dossier", "muon" ];
+            var items = 0;
+            _.each(str, function(s) { 
+                f.add(s); 
+            });
+            this.callback(null, {f:f, s:str});
+        },
+        'should have a picture with some Xs': function(x) {
+            
+            var f = x.f;
+            var pic = f.picture();
+            assert.equal(32,f.bits);
+            assert.isTrue(_.all(x.s, function(item) {
+                return f.test(item);
+            }));
+            assert.isTrue(/X/.test(pic));
+            console.log("|--------------------------------|");
+            console.log("|" + pic + "|");
+            console.log("|--------------------------------|");
+        },
+    },
+});
+
+v.export(module);
+
